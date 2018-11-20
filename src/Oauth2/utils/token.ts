1ad3c3af591788ed23fe52c300a5f36f1243dcd7
JacopoPatrocloMartinelli <jacopo.martinelli@farnedi.it>
2018-11-20 15:36:34 +0100

    fix refresh token

--- src/Oauth2/utils/token.ts
+++ src/Oauth2/utils/token.ts
@@ -2,10 +2,10 @@ import fetch from 'node-fetch';
 
 export default (options: { CLIENT_ID: string; CLIENT_SECRET: string }) => ({
   refreshToken: (refresh_token: string) =>
-    fetch('https://id.getharvest.com/oauth2/token', {
+    fetch('https://id.getharvest.com/api/v2/oauth2/token', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         refresh_token,
